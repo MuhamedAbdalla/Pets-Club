e21f0b2096b14a234e775a30d4d9b7db261777f9
MuhamedAbdalla <medokingdom7@outlook.com>
2021-04-13 15:27:29 +0200

    Fix user tests

--- microservices/user/src/controller/index.ts
+++ microservices/user/src/controller/index.ts
@@ -1,25 +1,23 @@
-import express from 'express';
-import { json } from 'body-parser';
-import { API } from '../config';
-import { addUser } from './user';
-export * from './user'
+import express from "express";
+import { json } from "body-parser";
+import { API } from "../config";
+import { addUser, getUser, login, updateUser } from "./user";
+export * from "./user";
 
 export const app = express();
 const PORT = process.env.PORT || API.PORT;
 
-app.use(json({limit: '50mb'}));
+app.use(json({ limit: "50mb" }));
 
 app.post(API.ABS_ENDPOINT_REGISTER, addUser);
 app.post(API.ABS_ENDPOINT_LOGIN, login);
 app.get(API.USER.ABS_ENDPOINT, getUser);
 app.put(API.USER.ABS_ENDPOINT, updateUser);
 
-
-if(process.env.NODE_ENV == 'PRODUCTION') {
+if (process.env.NODE_ENV == "PRODUCTION") {
     app.listen(PORT, () => {
-        console.log(`the service is running on port ${PORT}`)
-    });    
-}
-else {
+        console.log(`the service is running on port ${PORT}`);
+    });
+} else {
     app.listen();
 }
